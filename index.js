0fa91fdefee487ef522d746d52c88dad60a00a66
itsrohit2001 <144776346+itsrohit2001@users.noreply.github.com>
2023-10-11 18:05:15 +0530

    asynchronous javascript demo added

---
+++ index.js
@@ -0,0 +1,93 @@
+// $(document).ready( function(){
+//     $("h1").text("changed")
+// })
+
+// $(document).ready(function(){
+//     alert("Hello World!")
+// })
+
+$(document).ready(function(){
+    // $("#btnSave1").click(function(){
+    //     $("p").hide()
+
+    // })
+    // $("#btnSave2").click(function(){
+    //     $("p").show()
+    // })
+    // $("#btnSave1").click(function(){
+    //     $("p").fadeOut()
+    // })
+    // $("#btnSave2").click(function(){
+    //     $("p").fadeIn()
+    // })
+
+    // $("#btnSave1").click(function(){
+    //     $("p").toggle()
+    // })
+
+    // $("#btnSave1").click(function(){
+    //     $("#btnSave1").text("changed")
+    // })
+
+    // $("#btnSave1").click(function(){
+    //     $("#btnSave1").html("changed")
+    // })
+
+    // $("#btnSave1").click(function(){
+    //     $("#btnSave1").attr("changed")
+    // })
+
+    // $("ul").append("<li>appended")
+    // $("ul").prepend("<li>prepended")
+    // $("ul").before("<p>added before")
+    // $("ul").after("<p>added after")
+    // $("ul").wrap()
+
+    // $("li").empty()
+    // $("li.first").remove()
+
+    // $("ul.first").remove()
+
+    // $("#btnSave1").remove()
+
+    // $(".list-container").addClass("<li>class Added")
+
+    // $(".class-demo").on("mouseenter",function(){
+    //     $(this).addClass("border")
+
+    // })
+
+    // $(".class-demo").on("mouseleave",function(){
+    //     $(this).removeClass("border")
+    // })
+
+    // $(".class-demo").on("mouseenter",function(){
+    //     $(this).translate("20px")
+    // })
+
+    // $(".ul").on("mouseenter",function(){
+    //     $("li").parent().addClass("list-container")
+    // })
+
+    // $(".item1").parent().addClass("list-container")
+
+    // $("em").parent().addClass("em1")
+
+    // $("p").children().addClass("border")
+
+    $("#ul li").first().add("border","underline")
+
+
+    
+    
+    
+
+    
+
+
+
+
+
+
+
+})
